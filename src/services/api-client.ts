9a4a399cb67c550b8b52aee912bece77999391cd
Muminjon <juramirzaev.muminjon@gmail.com>
2023-12-08 20:27:37 +0100

    Building Game Cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '8ec47ff325ac457885ce17067d285228'
+    key: 'a61a77eb285f4460808b029d864b4b08'
   }
 })
