68fefeba6059b20d6802838bf37c2976fbc38571
Muminjon <juramirzaev.muminjon@gmail.com>
2023-12-14 15:27:04 +0100

    cleaning up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <p>{error}</p>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={5}
         spacing={3}
       >
